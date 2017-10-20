fe06ef57146e688a20cf38b7f4a22991941ecf9b
marudits <marudits@gmail.com>
2017-10-20 18:35:27 +0800

    :sparkles: NEW FEATURES | Add createdAtFormatted using calculateDiffTime utils

--- common/utils/dateAndTime.js
+++ common/utils/dateAndTime.js
@@ -20,5 +20,26 @@ module.exports = {
 			afterTime = moment(officeHours[1], format);
 
 		return time.isBetween(beforeTime, afterTime) && officeDays.indexOf(day) !== -1;
+	},
+
+	calculateDiffTime: function(time){
+		let diffTime = moment().diff(moment(time)),
+			sentTime = new Date(time);
+
+		let countDays =  moment.utc(diffTime).dayOfYear() - 1,
+			countHours = moment.utc(diffTime).hour(),
+			countMinutes = moment.utc(diffTime).minute();
+
+		if(countDays > 1){
+			return moment(sentTime).format('D MMM YYYY').toString();
+		} else if(countDays === 1){
+			return 'Yesterday';
+		} else {
+			if(countHours >= 1){
+				return `${countHours}h ago`;
+			} else {
+				return `${countMinutes}m ago`;
+			}
+		}
 	}
 }
