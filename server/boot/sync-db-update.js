ea6e30f9df3500bd626c900a444e33ad598bac93
marudits <marudits@gmail.com>
2017-11-01 14:56:26 +0800

    :wrench: FIXING | Fix db-sync-update mechanism and sample data

--- server/boot/sync-db-update.js
+++ server/boot/sync-db-update.js
@@ -2,32 +2,35 @@ var SAMPLE_DATA = require('../migrations/sample-data');
 
 module.exports = function(app) {
 	//const models = ['Bank', 'Rating', 'Department', 'Position', 'Employee', 'CallHistory'];
-	const models = ['Category', 'Reference', 'UserAction'];
-	const mode = 'autoupdate';
+	//const models = ['Reference'];
+	const mode = 'automigrate';
 	const dbConnector = 'dbpostgresqlheroku';
 
-	app.dataSources[dbConnector][mode](models, (err) => {
-		if(err){
-			throw err;
-		}
+	if(typeof models !== 'undefined' && models.length > 0){
+		app.dataSources[dbConnector][mode](models, (err) => {
+			if(err){
+				throw err;
+			}
 
-		for(let model of models){
-			console.log(`Model ${model} has been ${mode}`)
-		}
+			for(let model of models){
+				console.log(`Model ${model} has been ${mode}`)
+			}
 
-		if(mode === 'automigrate'){
+			if(mode === 'automigrate'){
 
-			for(let model of models) {
-				if(SAMPLE_DATA[model] && SAMPLE_DATA[model].length > 0){
-					app.models[model].create(SAMPLE_DATA[model], (err, res) => {
-						if(err){
-							throw err;
-						}
+				for(let model of models) {
+					if(SAMPLE_DATA[model] && SAMPLE_DATA[model].length > 0){
+						app.models[model].create(SAMPLE_DATA[model], (err, res) => {
+							if(err){
+								throw err;
+							}
 
-						console.log(`Models ${model} has been migrated!`);
-					});
+							console.log(`Models ${model} has been migrated!`);
+						});
+					}
 				}
 			}
-		}
-	});
+		});
+	}
+	
 }
--- server/migrations/sample-data.js
+++ server/migrations/sample-data.js
@@ -137,152 +137,202 @@ module.exports = {
 	],
 	'Reference': [
 		{
+			id: 1,
 			categoryId: 1,
 			value: 'API',
+			key: 'APP_TYPE_API',
 			type: [1,2,3],
 			description: 'API service'
 		},
 		{
+			id: 2,
 			categoryId: 1,
 			value: 'WEB',
+			key: 'APP_TYPE_WEB',
 			type: [1,2,3],
 			description: 'Web for Admin Page'
 		},
 		{
+			id: 3,
 			categoryId: 1,
 			value: 'MOBILE',
+			key: 'APP_TYPE_MOBILE',
 			type: [1,2,3],
 			description: 'Mobile application'
 		},
 		{
+			id: 4,
 			categoryId: 2,
 			value: 'NAVIGATE',
+			key: 'BANK_ACTIONS_NAVIGATE',
 			type: [1,3],
 			description: 'BANK_ACTIONS: NAVIGATE'
 		},
 		{
+			id: 5,
 			categoryId: 2,
 			value: 'CALL',
+			key: 'BANK_ACTIONS_CALL',
 			type: [1,3],
 			description: 'BANK_ACTIONS: CALL'
 		},
 		{
+			id: 6,
 			categoryId: 2,
 			value: 'REVIEW',
+			key: 'BANK_ACTIONS_REVIEW',
 			type: [1,3],
 			description: 'BANK_ACTIONS: REVIEW'
 		},
 		{
+			id: 7,
 			categoryId: 2,
 			value: 'FAVOURITE',
+			key: 'BANK_ACTIONS_FAVOURITE',
 			type: [1,3],
 			description: 'BANK_ACTIONS: FAVOURITE'
 		},
 		{
+			id: 8,
 			categoryId: 2,
 			value: 'ADD',
+			key: 'BANK_ACTIONS_ADD',
 			type: [1,2],
 			description: 'BANK_ACTIONS: ADD'
 		},
 		{
+			id: 9,
 			categoryId: 2,
 			value: 'UPDATE',
+			key: 'BANK_ACTIONS_UPDATE',
 			type: [1,2],
 			description: 'BANK_ACTIONS: UPDATE'
 		},
 		{
+			id: 10,
 			categoryId: 2,
 			value: 'DELETE',
+			key: 'BANK_ACTIONS_DELETE',
 			type: [1,2],
 			description: 'BANK_ACTIONS: DELETE'
 		},
 		{
+			id: 11,
 			categoryId: 2,
 			value: 'DETAIL',
+			key: 'BANK_ACTIONS_DETAIL',
 			type: [1,2],
 			description: 'BANK_ACTIONS: DETAIL'
 		},
 		{
+			id: 12,
 			categoryId: 3,
 			value: 'REPORT',
+			key: 'RATING_ACTIONS_REPORT',
 			type: [1,2],
 			description: 'RATING_ACTIONS: REPORT'
 		},
 		{
+			id: 13,
 			categoryId: 3,
 			value: 'ADD',
+			key: 'RATING_ACTIONS_ADD',
 			type: [1,3],
 			description: 'RATING_ACTIONS: ADD'
 		},
 		{
+			id: 14,
 			categoryId: 3,
 			value: 'DELETE',
+			key: 'RATING_ACTIONS_DELETE',
 			type: [1,2],
 			description: 'RATING_ACTIONS: DELETE'
 		},
 		{
+			id: 15,
 			categoryId: 3,
 			value: 'DETAIL',
+			key: 'RATING_ACTIONS_DETAIL',
 			type: [1,2],
 			description: 'RATING_ACTIONS: DETAIL'
 		},
 		{
+			id: 16,
 			categoryId: 4,
 			value: 'CALL',
+			key: 'CONTACT_ACTIONS_CALL',
 			type: [1,3],
 			description: 'CONTACT_ACTIONS: CALL'
 		},
 		{
+			id: 17,
 			categoryId: 4,
 			value: 'FAVOURITE',
+			key: 'CONTACT_ACTIONS_FAVOURITE',
 			type: [1,3],
 			description: 'CONTACT_ACTIONS: FAVOURITE'
 		},
 		{
+			id: 18,
 			categoryId: 4,
 			value: 'MAIL',
+			key: 'CONTACT_ACTIONS_MAIL',
 			type: [1,3],
 			description: 'CONTACT_ACTIONS: MAIL'
 		},
 		{
+			id: 19,
 			categoryId: 4,
 			value: 'ADD',
+			key: 'CONTACT_ACTIONS_ADD',
 			type: [1,2],
 			description: 'CONTACT_ACTIONS: ADD'
 		},
 		{
+			id: 20,
 			categoryId: 4,
 			value: 'UPDATE',
+			key: 'CONTACT_ACTIONS_UPDATE',
 			type: [1,2],
 			description: 'CONTACT_ACTIONS: UPDATE'
 		},
 		{
+			id: 21,
 			categoryId: 4,
 			value: 'DELETE',
+			key: 'CONTACT_ACTIONS_DELETE',
 			type: [1,2],
 			description: 'CONTACT_ACTIONS: DELETE'
 		},
 		{
+			id: 22,
 			categoryId: 4,
 			value: 'DETAIL',
+			key: 'CONTACT_ACTIONS_DETAIL',
 			type: [1,2],
 			description: 'CONTACT_ACTIONS: DETAIL'
 		},
 		{
+			id: 23,
 			categoryId: 5,
 			value: 'ANDROID',
+			key: 'PLATFORM_TYPE_ANDROID',
 			type: [1,2,3],
 			description: 'PLATFORM_TYPE: ANDROID'
 		},
 		{
+			id: 24,
 			categoryId: 5,
 			value: 'IOS',
+			key: 'PLATFORM_TYPE_IOS',
 			type: [1,2,3],
 			description: 'PLATFORM_TYPE: IOS'
 		},
 		{
+			id: 25,
 			categoryId: 5,
 			value: 'WEB',
+			key: 'PLATFORM_TYPE_WEB',
 			type: [1,2,3],
 			description: 'PLATFORM_TYPE: WEB'
 		}
